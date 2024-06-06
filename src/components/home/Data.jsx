fdb58b81dea447286e185ad35d2549fde83c61ec
Sineth Shashintha <sinethshashintha2000@gmail.com>
2024-06-06 15:14:19 +0530

    implementing home section

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+    <div className='home__data'>
+        <h1 className="home__title">
+            Sineth Shashintha
+        </h1>
+
+        <h3 className="home__subtitle">QA Engineer</h3>
+        <p className="home__description">I am a passionate 
+        Software Engineering undergraduate student at The Open University of Sri Lanka.</p>
+
+        <a href="#contact" className="button button--flex">Say Hello</a>
+      
+    </div>
+  )
+}
+
+export default Data
